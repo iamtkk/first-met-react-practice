0c33cc91fd2652209ecf457a653aae765b8a4d44
iamtkk <iamtkk2@gmail.com>
2022-09-18 19:17:23 +0900

    TIL/React/220918

--- playground/my-app/src/index.js
+++ playground/my-app/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import Library from './chapter_03/Library';
+import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -13,23 +16,21 @@ import Clock from './chapter_04/Clock';
 //   </React.StrictMode>
 // );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-}, 1000);
-
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 // setInterval(() => {
-//   ReactDOM.render(
+//   root.render(
 //     <React.StrictMode>
-//       <Clock />
-//     </React.StrictMode>,
-//     document.getElementById('root')
+//       <CommentList />
+//     </React.StrictMode>
 //   );
 // }, 1000);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Accommodate />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

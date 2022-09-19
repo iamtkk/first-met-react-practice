3d531800762121c30f53c58312cf2c5904128d49
김태관 <iamtk@neowiz.com>
2022-09-19 20:17:20 +0900

    TIL/React/220919

--- playground/my-app/src/index.js
+++ playground/my-app/src/index.js
@@ -10,6 +10,7 @@ import NotificationList from "./chapter_06/NotificationList";
 import Accommodate from "./chapter_07/Accommodate";
 import ConfirmButton from "./chapter_08/ConfirmButton";
 import LandingPage from "./chapter_09/LandingPage";
+import AttendanceBook from "./chapter_10/AttendanceBook";
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -29,7 +30,7 @@ import LandingPage from "./chapter_09/LandingPage";
 
 ReactDOM.render(
   <React.StrictMode>
-    <LandingPage />
+    <AttendanceBook />
   </React.StrictMode>,
   document.getElementById("root")
 );

f6453db0f1e0cc30a13391258c54e91c2651144a
김태관 <iamtk@neowiz.com>
2022-09-19 14:12:14 +0900

    TIL/React/220919

--- playground/my-app/src/index.js
+++ playground/my-app/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Library from './chapter_03/Library';
-import Clock from './chapter_04/Clock';
-import CommentList from './chapter_05/CommentList';
-import NotificationList from './chapter_06/NotificationList';
-import Accommodate from './chapter_07/Accommodate';
-import ConfirmButton from './chapter_08/ConfirmButton';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Library from "./chapter_03/Library";
+import Clock from "./chapter_04/Clock";
+import CommentList from "./chapter_05/CommentList";
+import NotificationList from "./chapter_06/NotificationList";
+import Accommodate from "./chapter_07/Accommodate";
+import ConfirmButton from "./chapter_08/ConfirmButton";
+import LandingPage from "./chapter_09/LandingPage";
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -28,9 +29,9 @@ import ConfirmButton from './chapter_08/ConfirmButton';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ConfirmButton />
+    <LandingPage />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
